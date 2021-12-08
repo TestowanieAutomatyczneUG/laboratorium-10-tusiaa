a869efd9d6368a6df07077142805cd43ff757f8b
tusiaa <sandra.leman146@gmail.com>
2021-12-08 21:07:48 +0100

    Added setUp and tearDown

--- tests/Tests_NotesService.py
+++ tests/Tests_NotesService.py
@@ -5,29 +5,31 @@ from src.NotesService import *
 
 class TestNoteService(unittest.TestCase):
 
+    def setUp(self):
+        self.test_object = NotesService()
+
     @patch.object(NotesStorage, 'getAllNotesOf')
     def test_averageOf(self, mock_method):
         mock_method.return_value = [Note("note", 3), Note("note", 4), Note("note", 5)]
-        test_object = NotesService()
-        result = test_object.averageOf("note")
+        result = self.test_object.averageOf("note")
         self.assertEqual(4, result, 'return value incorrect')
 
     @patch.object(NotesStorage, 'getAllNotesOf')
     def test_averageOf_0Notes(self, mock_method):
         mock_method.return_value = []
-        test_object = NotesService()
-        result = test_object.averageOf("note")
+        result = self.test_object.averageOf("note")
         self.assertEqual(0, result, 'return value incorrect')
 
     @patch.object(NotesStorage, 'add')
     def test_add(self, mock_method):
         note = Note("note", 3)
-        test_object = NotesService()
-        test_object.add(note)
+        self.test_object.add(note)
         mock_method.assert_called_once_with(note)
 
     @patch.object(NotesStorage, 'clear')
     def test_clear(self, mock_method):
-        test_object = NotesService()
-        test_object.clear()
+        self.test_object.clear()
         mock_method.assert_called_once_with()
+
+    def tearDown(self):
+        del self.test_object
